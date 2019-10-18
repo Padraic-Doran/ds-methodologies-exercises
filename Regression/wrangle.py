59844b8213a2232c4eb2de0692d8ba26c606a810
Padraic Doran <padraic.doran@gmail.com>
2019-10-18 09:12:24 -0500

    Regression

--- Regression/wrangle.py
+++ Regression/wrangle.py
@@ -1,97 +1,122 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[5]:
+# In[1]:
 
 
 get_ipython().run_line_magic('matplotlib', 'inline')
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from env import user, host, password
+import env
 from scipy import stats
 import csv
 
 np.random.seed(29)
 
 
-# In[12]:
+# In[ ]:
 
 
-def get_db_url(username, hostname, password, db_name):
-    return f'mysql+pymysql://{username}:{password}@{hostname}/{db_name}'
+def get_db_url(db):
+    return f'mysql+pymysql://{env.user}:{env.password}@{env.host}/{db}'
 
-query = '''
-   Select `customer_id`, monthly_charges, `total_charges`, tenure from customers
-Where contract_type_id = 3
-;
-'''
+    
+def get_data_from_mysql():
+    query = '''
+    Select props.`id` as property_id, 
+    props.`bathroomcnt` as bathroom_count,
+    props.`bedroomcnt` as bedroom_count,
+    props.`calculatedfinishedsquarefeet` as calc_finish_sq_ft,
+    props.`taxvaluedollarcnt` as assessed_property_value,
+    props.`taxamount` as tax_paid,
+    (props.`taxamount` / props.`taxvaluedollarcnt`) * 100 as tax_rate,
+    props.`fips` as county_code,
+    svi.`COUNTY` as county
+    from zillow.properties_2017 as props
+        JOIN zillow.predictions_2017 as pred
+        on zillow.props.id = zillow.pred.id
+    JOIN svi_db.`svi2016_us_county` as svi
+    on zillow.props.fips = svi_db.svi.fips
+    Where transactiondate like "2017-05%%" or transactiondate like "2017-06%%" 
+    and props.`propertylandusetypeid` not in (31, 246, 247, 248)
+    AND
+    props.`calculatedfinishedsquarefeet` IS NOT NULL
+    AND 
+    props.`bathroomcnt` != 0
+    AND
+    props.`bedroomcnt` != 0
 
-url = get_db_url(user,host,password,'telco_churn')
-charges = pd.read_sql(query,url)
-charges.head()
+    '''
+
+    df = pd.read_sql(query, get_db_url('zillow'))
+    return df    
+   
+    
+df = get_data_from_mysql()
+
+df.head()
 
 
-# In[13]:
+# In[ ]:
 
 
-charges.info()
+def clean_data(df):
+    df = df.dropna()
+    df = df[df.bathroom_count > 0]
+    df = df[df.bedroom_count > 0]
+    return df
 
 
-# In[21]:
+# In[ ]:
 
 
-charges.total_charges.value_counts(sort=True, ascending=False)
+def wrangle_zillow():
+    df = get_data_from_mysql()
+    df = clean_data(df)
+    return df
 
 
 # In[24]:
 
 
-charges.total_charges.replace(r'^\s*$', np.nan, regex=True, inplace=True)
 
 
-# In[25]:
+
+# In[ ]:
 
 
-charges.info()
+
 
 
 # In[27]:
 
 
-charges = charges.dropna()
 
 
-# In[32]:
+
+# In[ ]:
 
 
-charges.total_charges = charges.total_charges.astype(float)
-charges.total_charges
-charges.info()
 
 
-# In[33]:
+
+# In[67]:
 
 
-def wrangle_telco():
-    query = '''
-       Select `customer_id`, monthly_charges, `total_charges`, tenure from customers
-    Where contract_type_id = 3
-    ;
-    '''
 
-    url = get_db_url(user,host,password,'telco_churn')
-    charges = pd.read_sql(query,url)
-    charges.total_charges.replace(r'^\s*$', np.nan, regex=True, inplace=True)
-    charges = charges.dropna()
-    charges.total_charges = charges.total_charges.astype(float)
-    return charges
 
 
 # In[34]:
 
 
 
+
+
+# In[ ]:
+
+
+wrangle_zillow()
 
 
 # In[ ]:
